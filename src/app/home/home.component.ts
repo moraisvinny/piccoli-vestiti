fc16eb045bc72ff7464151841bc25feb4aff5e51
Vinicius Morais <vinicius.reis.morais@gmail.com>
2018-01-24 22:30:15 -0200

    formulario de contato

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,19 @@ import { ProdutoService } from '../produto.service';
 import { Produto } from '../shared/models/produto-model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+
+interface MsgErro {
+  nome: string,
+  email: string,
+  mensagem: string
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
 
@@ -15,6 +23,8 @@ export class HomeComponent implements OnInit {
   public produtos: Produto[] = []
   public produtoModal: Produto
   public contatoForm: FormGroup
+  public msgErro: MsgErro = {nome: undefined, email: undefined,  mensagem:undefined}
+  
 
   constructor(private produtoService: ProdutoService, private fb: FormBuilder) {
 
@@ -31,20 +41,22 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  public testaForm() {
+    console.log("FORM : ", this.contatoForm.controls)
+  }
+
   public loadScript() {
     var isFound = false;
     var scripts = document.getElementsByTagName("script")
     for (var i = 0; i < scripts.length; ++i) {
-      if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
+      if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("freelancer")) {
         isFound = true;
       }
     }
 
     if (!isFound) {
       var dynamicScripts = [
-        "assets/js/freelancer.js",
-        "assets/js/contact_me.min.js",
-        "assets/js/jqBootstrapValidation.min.js"];
+        "assets/js/freelancer.js"];
 
       for (var i = 0; i < dynamicScripts.length; i++) {
         let node = document.createElement('script');
@@ -62,11 +74,11 @@ export class HomeComponent implements OnInit {
 
     emailjs.init("user_PDafcdu4DrQAbTzFfhmzj");
 
-    this.fb.group({
+    this.contatoForm = this.fb.group({
       nome:["", [Validators.required, Validators.minLength(3)]],
       email: ["", [Validators.required, Validators.email]],
       telefone: ["", [Validators.required]],
-      mensagem: [""],
+      mensagem: ["", [Validators.required]],
     })
 
     this.produtoService.listarProdutos((snapshot) => {
@@ -97,19 +109,50 @@ export class HomeComponent implements OnInit {
     window.open(link, '_blank')
   }
 
+  private validaForm(): boolean {
+
+    let isFormValido:boolean = true
+    this.msgErro = {nome: undefined, email: undefined, mensagem:undefined}
+
+    if(this.contatoForm.get('nome').errors) {
+      this.msgErro.nome = "Nome inválido"
+      isFormValido = false
+    }
+
+    if(this.contatoForm.get('email').errors) {
+      this.msgErro.email = "Email inválido"
+      isFormValido = false
+    }
+
+    if(this.contatoForm.get('mensagem').errors) {
+      this.msgErro.mensagem = "Por favor, digite uma mensagem."
+      isFormValido = false
+    }
+    return isFormValido;
+    
+  }
+
   public enviaContato(): void {
 
-    emailjs.send("gmail", "template_4XRS9BZI", {
-      nome: "TESTE VINICIUS",
-      email: "TESTE@TESTE",
-      telefone: "1234",
-      mensagem: "Mensagem de teste"
-    }).then((response) => {
-      console.log("Deu certo!");
-    })
-      .catch((err) => {
-        console.log("Deu erro!");
-      });
+    if(this.validaForm()) {
+      emailjs.send("gmail", "template_4XRS9BZI", {
+        nome: this.contatoForm.value.nome,
+        email: this.contatoForm.value.email,
+        telefone: this.contatoForm.value.telefone,
+        mensagem: this.contatoForm.value.mensagem
+      }).then((response) => {
+        // TODO Modal de retorno do envio do email
+      })
+        .catch((err) => {
+          console.log("Deu erro!");
+        });
+    }
+
+  }
+
+  public limpaErro(campo: string): void {
+    
+    this.msgErro[campo] = undefined
   }
 
 }
